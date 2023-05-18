8f026ae9c0fd5fb8c59440d7afd7f63089024dec
SaptatirthaChoudhury <saptatirtha@gmail.com>
2023-05-18 21:12:32 +0530

    Server setup

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+
+
+
+
+
+
+
+app.listen(8000, () => {         // TODO : Move this port num to the config folder and .env file
+    console.log("Server started");
+})
